4ea7fe5ba8e5bca107b29829e341ac7f3e753ee3
Jero Graveson <jero.graveson@springer.com>
2008-04-08 04:32:37 +0000

    moved DB access to repository class

--- server/Adis.Log.Server/LoggerImplementer.cs
+++ server/Adis.Log.Server/LoggerImplementer.cs
@@ -9,103 +9,30 @@ namespace Adis.Log.Server
 {
 	internal class LoggerImplementer
 	{
-		private Adis.Log.Server.LoggerDataContext dbContext;
-
-		public LoggerImplementer()
+		/// <summary>
+		/// Inserts a new logObject into the database
+		/// </summary>
+		/// <param name="logObject"></param>
+		/// <returns></returns>
+		public bool InsertNewLog(LogTransportObject logObject, IRepository repository)
 		{
 			ILog internalLog = LogManager.GetLogger(this.GetType());
-			try
-			{
-				internalLog.DebugFormat("Attempting to connect to database. ConnectionString is {0}", 
-					global::Adis.Log.Properties.Settings.Default.LoggerConnectionString);
-				dbContext = new Adis.Log.Server.LoggerDataContext();
-			}
-			catch (Exception e)
-			{
-				internalLog.Error("Failed to create LoggerDatabaseContext", e);
-				throw;
-			}
-		}
+			internalLog.DebugFormat("Attempting to insert a new log. App={0} Severity={1}", logObject.Application, logObject.Severity);
 
-		~LoggerImplementer()
-		{
-			dbContext.Dispose();
-		}
-
-		public bool InsertNewLog(LogTransportObject logObject)
-		{
-			ILog internalLog = LogManager.GetLogger(this.GetType());
 			LogEvent logEvent = (LogEvent)logObject;
 			try
 			{
-				lock (dbContext)
-				{
-					dbContext.LogEvents.InsertOnSubmit(logEvent);
-
-					dbContext.SubmitChanges();
-				}
+				repository.InsertIntoDatabase(logEvent);
 			}
 			catch (Exception e)
 			{
+#if DEBUG
 				System.Diagnostics.Debug.WriteLine(e.ToString());
+#endif
 				internalLog.Error("Failed to insert new log into the database", e);
 			}
 			return true;
 		}
 
-		private static LogEvent GenerateLogEvent(LogTransportObject logObject)
-		{
-			ILog internalLog = LogManager.GetLogger(typeof(LoggerImplementer));
-			LogEvent logEvent = new LogEvent()
-			{
-				Application	= logObject.Application,
-				Category		= logObject.Category,
-				EventTime		= logObject.Time,
-				ExtraInfo		= logObject.ExtraInfo,
-				Instance		= logObject.Instance,
-				Machine			= logObject.Machine,
-				Message			= logObject.Message,
-				Severity		= logObject.Severity,
-				TimeLogged	= DateTime.Now,
-				User				= logObject.User
-			};
-
-			int MaxApplicationLength	= 256;
-			int MaxCategoryLength			= 64;
-			int MaxInstanceLength			= 256;
-			int MaxMachineLength			= 256;
-			int MaxMessageLength			= 2048;
-			int MaxSeverityLength			= 5;
-			int MaxUserLength					= 256;
-			if (logEvent.Application.Length > MaxApplicationLength)
-			{
-				logEvent.Application = logEvent.Application.Substring(0, MaxApplicationLength);
-			}
-			if (logEvent.Category.Length > MaxCategoryLength)
-			{
-				logEvent.Category = logEvent.Category.Substring(0, MaxCategoryLength);
-			}
-			if (logEvent.Instance.Length > MaxInstanceLength)
-			{
-				logEvent.Instance = logEvent.Instance.Substring(0, MaxInstanceLength);
-			}
-			if (logEvent.Machine.Length > MaxMachineLength)
-			{
-				logEvent.Machine = logEvent.Machine.Substring(0, MaxMachineLength);
-			}
-			if (logEvent.Message.Length > MaxMessageLength)
-			{
-				logEvent.Message = logEvent.Message.Substring(0, MaxMessageLength);
-			}
-			if (logEvent.Severity.Length > MaxSeverityLength)
-			{
-				logEvent.Severity = logEvent.Severity.Substring(0, MaxSeverityLength);
-			}
-			if (logEvent.User.Length > MaxUserLength)
-			{
-				logEvent.User = logEvent.User.Substring(0, MaxUserLength);
-			}
-			return logEvent;
-		}
 	}
 }
