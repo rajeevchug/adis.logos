e1faed41d3cbce1a5db70bdfe7693202e266212d
Jero Graveson <jero.graveson@springer.com>
2008-04-08 04:43:53 +0000

    Added Identifier for the main (UI) thread

--- listeners/Adis.Log.Listener.WinForms/Program.cs
+++ listeners/Adis.Log.Listener.WinForms/Program.cs
@@ -8,12 +8,17 @@ namespace Adis.Log.Listener.WinForms
 	static class Program
 	{
 		public static MainForm mainForm;
+
+		public static int UiThreadId { get; set; }
+
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
 		static void Main()
 		{
+			UiThreadId = System.Threading.Thread.CurrentThread.ManagedThreadId;
+
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 
