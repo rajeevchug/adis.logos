2f169b9a23de71163ad29bbb4b1fd8dc802c5245
Jero Graveson <jero.graveson@springer.com>
2008-06-03 04:31:33 +0000

    - GetRecords() uses extension methods on IQueryable<> now - filtering done on Maximum severity now instead of exact severity - ID filter added

--- server/Adis.Log.Server/ReporterImplementer.cs
+++ server/Adis.Log.Server/ReporterImplementer.cs
@@ -29,20 +29,24 @@ namespace Adis.Log.Server
 		{
 			ILog internalLog = LogManager.GetLogger(typeof(ReporterImplementer));
 			IQueryable<LogEvent> logEvents = repository.GetAllLogLogEvents();
-			logEvents = AddApplicationFilter(filter, logEvents);
-			logEvents = AddCategoryFilter(filter, logEvents);
-			logEvents = AddInstanceFilter(filter, logEvents);
-			logEvents = AddMachineFilter(filter, logEvents);
-			logEvents = AddUserFilter(filter, logEvents);
-			logEvents = AddSeverityFilter(filter, logEvents);
+
+			logEvents = logEvents.WithId(filter.Id)
+				.WithApplication(filter.Application, filter.ApplicationExactMatch)
+				.WithCategory(filter.Category, filter.CategoryExactMatch)
+				.WithInstance(filter.Instance, filter.InstanceExactMatch)
+				.WithMachine(filter.Machine, filter.MachineExactMatch)
+				.WithUser(filter.User, filter.UserExactMatch)
+				.WithMaxSeverity(filter.Severity)
+				.WithMessage(filter.Message, filter.MessageExactMatch)
+				;
 
 			if (useTimeLoggedNotEventTime)
 			{
-				logEvents = AddTimeLoggedFilter(filter, logEvents);
+				logEvents = logEvents.WithTimeLoggedInRange(filter.StartTime, filter.EndTime);
 			}
 			else
 			{
-				logEvents = AddEventTimeFilter(filter, logEvents);
+				logEvents = logEvents.WithEventTimeInRange(filter.StartTime, filter.EndTime);
 			}
 
 			logEvents = logEvents.Skip(skipFirst);
@@ -51,7 +55,7 @@ namespace Adis.Log.Server
 				logEvents = logEvents.Take(maxRecords);
 			}
 
-			List<LogTransportObject> returnVal = new List<LogTransportObject>(logEvents.Count());
+			List<LogTransportObject> returnVal = new List<LogTransportObject>();
 			foreach (LogEvent log in logEvents)
 			{
 				returnVal.Add(log.ToLogTransportObject());
@@ -62,131 +66,5 @@ namespace Adis.Log.Server
 			return returnVal;
 		}
 
-		#region Private helper methods
-
-		private static IQueryable<LogEvent> AddSeverityFilter(RequestFilter filter, IQueryable<LogEvent> logEvents)
-		{
-			if (filter.Severity != null)
-			{
-				logEvents = logEvents.Where(le => le.Severity.ToLower() == filter.Severity.ToLower());
-			}
-			return logEvents;
-		}
-
-		///Add where clause to filter out records whose eventTime isn't between the filter object's
-		///start and end times
-		private static IQueryable<LogEvent> AddEventTimeFilter(RequestFilter filter, IQueryable<LogEvent> logEvents)
-		{
-			if (filter.StartTime != null)
-			{
-				logEvents = logEvents.Where(le => le.EventTime >= filter.StartTime);
-			}
-			if (filter.EndTime != null)
-			{
-				logEvents = logEvents.Where(le => le.EventTime <= filter.EndTime);
-			}
-			return logEvents;
-		}
-
-		///Add where clause to filter out records whose eventTime isn't between the filter object's
-		///start and end times
-		private static IQueryable<LogEvent> AddTimeLoggedFilter(RequestFilter filter, IQueryable<LogEvent> logEvents)
-		{
-			if (filter.StartTime != null)
-			{
-				logEvents = logEvents.Where(le => le.TimeLogged >= filter.StartTime);
-			}
-			if (filter.EndTime != null)
-			{
-				logEvents = logEvents.Where(le => le.TimeLogged <= filter.EndTime);
-			}
-			return logEvents;
-		}
-
-		///Add where clause to filter out records based on the application field
-		private static IQueryable<LogEvent> AddApplicationFilter(RequestFilter filter, IQueryable<LogEvent> logEvents)
-		{
-			if (filter.Application != null)
-			{
-				if (filter.ApplicationExactMatch)
-				{
-					logEvents = logEvents.Where(le => le.Application == filter.Application);
-				}
-				else
-				{
-					logEvents = logEvents.Where(le => le.Application.StartsWith(filter.Application));
-				}
-			}
-			return logEvents;
-		}
-
-		///Add where clause to filter out records based on the category field
-		private static IQueryable<LogEvent> AddCategoryFilter(RequestFilter filter, IQueryable<LogEvent> logEvents)
-		{
-			if (filter.Category != null)
-			{
-				if (filter.CategoryExactMatch)
-				{
-					logEvents = logEvents.Where(le => le.Category == filter.Category);
-				}
-				else
-				{
-					logEvents = logEvents.Where(le => le.Category.StartsWith(filter.Category));
-				}
-			}
-			return logEvents;
-		}
-
-		///Add where clause to filter out records based on the instance field
-		private static IQueryable<LogEvent> AddInstanceFilter(RequestFilter filter, IQueryable<LogEvent> logEvents)
-		{
-			if (filter.Instance != null)
-			{
-				if (filter.InstanceExactMatch)
-				{
-					logEvents = logEvents.Where(le => le.Instance == filter.Instance);
-				}
-				else
-				{
-					logEvents = logEvents.Where(le => le.Instance.StartsWith(filter.Instance));
-				}
-			}
-			return logEvents;
-		}
-
-		///Add where clause to filter out records based on the machine field
-		private static IQueryable<LogEvent> AddMachineFilter(RequestFilter filter, IQueryable<LogEvent> logEvents)
-		{
-			if (filter.Machine != null)
-			{
-				if (filter.MachineExactMatch)
-				{
-					logEvents = logEvents.Where(le => le.Machine == filter.Machine);
-				}
-				else
-				{
-					logEvents = logEvents.Where(le => le.Machine.StartsWith(filter.Machine));
-				}
-			}
-			return logEvents;
-		}
-
-		///Add where clause to filter out records based on the user field
-		private static IQueryable<LogEvent> AddUserFilter(RequestFilter filter, IQueryable<LogEvent> logEvents)
-		{
-			if (filter.User != null)
-			{
-				if (filter.UserExactMatch)
-				{
-					logEvents = logEvents.Where(le => le.User == filter.User);
-				}
-				else
-				{
-					logEvents = logEvents.Where(le => le.User.StartsWith(filter.User));
-				}
-			}
-			return logEvents;
-		}
-		#endregion
 	}
 }
