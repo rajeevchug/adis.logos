31ec7721313af70661d2347f587551e29c7a5125
Jero Graveson <jero.graveson@springer.com>
2010-01-04 20:08:27 +0000

    added

---
+++ Reporter/Adis.Log.Reporter.MVC/Providers/ICacheProvider.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Adis.Log.Reporter.MVC.Providers
+{
+	interface ICacheProvider
+	{
+		void Add(string key, object value, TimeSpan expiry);
+		object this[string name] { get; set; }
+	}
+}
---
+++ Reporter/Adis.Log.Reporter.MVC/Providers/WebCacheProvider.cs
@@ -0,0 +1,34 @@
+﻿using System.Web;
+using System.Web.Caching;
+using System;
+
+namespace Adis.Log.Reporter.MVC.Providers
+{
+	public sealed class WebCacheProvider : ICacheProvider
+	{
+		private static Cache Cache
+		{
+			get { return HttpContext.Current.Cache; }
+		}
+
+		public static bool Contains(string name)
+		{
+			return Cache[name] != null;
+		}
+
+		public void Add(string key, object value, TimeSpan expiry)
+		{
+			Cache.Add(key, value, null, System.Web.Caching.Cache.NoAbsoluteExpiration, expiry, CacheItemPriority.Normal, null);
+		}
+
+		object this[string name]
+		{
+			get { return Cache[name]; }
+			set
+			{
+				if (value == null) Cache.Remove(name);
+				else Cache[name] = value;
+			}
+		}
+	}
+}
