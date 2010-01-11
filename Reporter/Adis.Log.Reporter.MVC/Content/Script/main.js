3094e83b13431375b3009b0b417b1c18e0d74916
Jero Graveson <jero.graveson@springer.com>
2010-01-11 02:33:15 +0000

    now the loading of categories and applications passes the log server name back to the web server instead of letting the web server decide it from cookies. This is so that it will still work in situations where the log server cookie is blank (eg first time use of web page)

--- Reporter/Adis.Log.Reporter.MVC/Content/Script/main.js
+++ Reporter/Adis.Log.Reporter.MVC/Content/Script/main.js
@@ -31,14 +31,24 @@
 		, FilterBarOnReady: function()
 		{
 			$('#category').change(function() { local.LoadApplicationFilterDropdown(); });
+			$('#logServer').change(function() { local.LoadCategoryFilterDropdown(); });
 			$('.log_item.has_extra_info').click(function() { logging.main.ToggleExtraInfo(this); });
-
+			//$('.filter_header').click(function() { logging.main.ToggleFilterSlide(); });
 		}
 
 		, SetPageNumberAndSubmit: function(pageNumber)
 		{
 			$('#pageNumber').val(pageNumber);
 			document.forms[0].submit();
+		}
+
+		, ToggleFilterSlide: function()
+		{
+			$('#filterForm').slideToggle('slow', function()
+			{
+				var i = 0;
+				$(".main_section").css('top', $(".top_section").height());
+			});
 		}
 
 		, ToggleExtraInfo: function(logItem)
@@ -56,13 +66,15 @@
 		, LoadCategoryFilterDropdown: function()
 		{
 			var currentCategory = $('#category').val();
-
+			
+			var logServer = $('#logServer').val();
+			
 			var $categoryDropdown = $("#category");
 			$categoryDropdown.unbind('change');
 			$categoryDropdown.html("<option value='-1'>Loading, please wait...</option>");
 
 			$.ajax({
-				url: urls.Categories,
+				url: urls.Categories.replace('__server__', logServer),
 				type: "GET",
 				dataType: "json",
 				async: true,
@@ -88,12 +100,13 @@
 		, LoadApplicationFilterDropdown: function()
 		{
 			var category = $("#category :selected").text();
+			var logServer = $('#logServer').val();
 
 			var $applicationDropdown = $("#application");
 			$applicationDropdown.html("<option value='-1'>Loading, please wait...</option>");
 
 			$.ajax({
-				url: urls.Applications.replace("__category__", category),
+				url: urls.Applications.replace("__server__", logServer).replace("__category__", category),
 				type: "GET",
 				dataType: "json",
 				async: true,
@@ -112,7 +125,54 @@
 
 		}
 
+		, ResetFilterCookies: function()
+		{
+			EraseCookie("LogServer");
+			EraseCookie("Category");
+			EraseCookie("Application");
+			EraseCookie("Severity");
+			EraseCookie("Machine");
+			EraseCookie("Instance");
+			EraseCookie("User");
+			EraseCookie("StartTime");
+			EraseCookie("EndTime");
+
+		}
+
 	}
+
+	function CreateCookie(name, value, days)
+	{
+		if (!days) { days = 365; }
+		var date = new Date();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+		var expires = "; expires=" + date.toGMTString();
+		document.cookie = name + "=" + value + expires + "; path=/";
+	}
+
+	function EraseCookie(name)
+	{
+		CreateCookie(name, "", -1);
+	}
+
+	function ReadCookie(name)
+	{
+		var nameEQ = name + "=";
+		var carr = document.cookie.split(';');
+		for (var i = 0; i < carr.length; i++)
+		{
+			var c = carr[i];
+			while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+			if (c.indexOf(nameEQ) == 0)
+			{
+				var cookie = c.substring(nameEQ.length, c.length);
+				log.debug(c);
+				return cookie;
+			}
+		}
+		return null;
+	}
+
 
 	// set up global object instance
 	if (!window.logging) { window.logging = new function() { } }
